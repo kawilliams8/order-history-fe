8b6391e91b033b79707e83ec255bf433b3269319
Katie Williams <kawilliams8@gmail.com>
2019-08-29 09:27:16 -0600

    First fetch response in state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../App/App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      orders: [],
+      error: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/purchases')
+    .then(response => response.json())
+    .then(data => this.setState({orders: data}))
+    .catch(error => this.setState({error.message}))
+  }
+
   render() {
+    console.log('render')
     return (
       <div className="App">
         <header>
