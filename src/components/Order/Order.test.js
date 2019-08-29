04594125916f8945dd5734d295e61fbd2c42eb11
Katie Williams <kawilliams8@gmail.com>
2019-08-29 11:35:37 -0600

    Add more method testing

--- src/components/Order/Order.test.js
+++ src/components/Order/Order.test.js
@@ -4,16 +4,29 @@ import Order from '../Order/Order';
 
 describe('Order', () => {
 
-  it('should match the snapshot', () => {
-    const mockOrder = {
+  let mockOrder;
+  beforeEach(() => {
+    mockOrder = {
       id: 1,
       img: "image.png",
       name: "Mock Order",
       description: "This is a mock description",
       price: 3
     }
-    const wrapper = shallow(<Order data={mockOrder} key={1}/>)
+  })
+
+  it('should match the snapshot', () => {
+    const removeOrderMock = jest.fn();
+    const wrapper = shallow(<Order data={mockOrder} key={1} removeOrder={removeOrderMock} />)
     expect(wrapper).toMatchSnapshot()
   });
+
+  it('should call the removeOrder method and pass the id', () => {
+    const removeOrderMock = jest.fn();
+    const wrapper = shallow(<Order data={mockOrder} key={1} removeOrder={removeOrderMock}/>)
+    wrapper.find('button').simulate('click')
+    expect(removeOrderMock).toHaveBeenCalled();
+    expect(removeOrderMock).toHaveBeenCalledWith(1);
+  })
 
 })
